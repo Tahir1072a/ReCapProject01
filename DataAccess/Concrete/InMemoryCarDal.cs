12473bad8df6e46f4a24e157afd57b774cb746bf
Tahiri Fidan <tahirifdn@gmail.com>
2022-08-19 21:30:00 +0300

    InMemoryYapısı oluşturuldu

---
+++ DataAccess/Concrete/InMemoryCarDal.cs
@@ -0,0 +1,53 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>() 
+            { 
+                new Car{CarId = 1,BrandId = 1, ColorId = 1,DailyPrice = 90, ModelYear = "2002", Description ="Kazasız temiz Araba"}
+            
+            };
+        }
+        
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(int id)
+        {
+            _cars.Remove(_cars.SingleOrDefault(p => p.CarId == id));
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;   
+        }
+
+        public List<Car> GetById(int id)
+        {
+            return _cars.Where(p => p.BrandId == id).ToList();
+        }
+
+        public void Update(Car carId)
+        {
+           var carToUpdate =  _cars.SingleOrDefault(p => p.CarId == carId.CarId);
+           carToUpdate.BrandId = carId.BrandId;
+           carToUpdate.ModelYear = carId.ModelYear;
+           carToUpdate.Description = carId.Description;
+           carToUpdate.DailyPrice = carId.DailyPrice;
+           carToUpdate.ColorId = carId.ColorId;
+        }
+    }
+}
---
+++ UI/Program.cs
@@ -0,0 +1,30 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace UI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.CarId = 2;
+            car.BrandId = 3;
+            car.Description = "Aslanapa";
+            car.DailyPrice = 900;
+            car.ColorId = 2;
+            car.ModelYear = "2000";
+            ICarService carService = new CarManager(new InMemoryCarDal());
+            foreach (var item in carService.GetAll())
+            {
+                Console.WriteLine(item.ModelYear);
+            }
+
+            carService.Add(car);
+            carService.Delete(1);
+        }
+    }
+}
